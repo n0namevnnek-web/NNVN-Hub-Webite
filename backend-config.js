f99c537c6840474f62b1c0d35d60c1934989bff7
N0NAMEVN <n0namevnnek@gmail.com>
2026-08-23 18:21:37 +0700

    Configure Supabase project

--- backend-config.js
+++ backend-config.js
@@ -1,4 +1,4 @@
 window.NNVN_BACKEND = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://ggkctzevloxlvmkitgld.supabase.co",
+  supabaseAnonKey: "sb_publishable_G8qV6XVVJBg3haZa6Fax7g_5vlftDLh"
 };
